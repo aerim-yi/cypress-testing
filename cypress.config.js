b2c096a368585ab3c2967cefd1fbb164fb99ec56
aerim-yi <aerim.yi@immutable.com>
2022-07-27 22:21:47 +1000

    feat: add function to handle reCAPTCHA iframes

--- cypress.config.js
+++ cypress.config.js
@@ -4,11 +4,12 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
-
+  chromeWebSecurity: false,
   e2e: {
     baseUrl: 'https://master-dev.website.godsunchained.com/',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     excludeSpecPattern: ['**/1-getting-started/*', '**/2-advanced-examples/*'],
+    experimentalSessionAndOrigin: true,
     setupNodeEvents(on, config) {
       config.env = process.env
       return config
