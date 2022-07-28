332d2e48e77032331710df2dcf417f762c160bb6
Chanel Donahoe <chanel.donahoe@immutable.com>
2022-07-29 09:10:49 +1000

    changes

--- cypress.config.js
+++ cypress.config.js
@@ -10,7 +10,6 @@ module.exports = defineConfig({
     baseUrl: 'https://master-dev.website.godsunchained.com/',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     excludeSpecPattern: ['**/1-getting-started/*', '**/2-advanced-examples/*'],
-    experimentalSessionAndOrigin: true,
     setupNodeEvents(on, config) {
       config.env = process.env
       return config
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,14 +1,15 @@
-import cypress from "cypress";
-import { contains } from "cypress/types/jquery";
+
 
 const recapture = require('../commands/recapture')
 
 Cypress.Commands.add('recapture', recapture)
 
-// Cypress.Commands.add('navigateNavBar', (page) => {
-//   cy.get('[data-test-id="navbarMainMenu"]').contains(page).parent().as('page');
-//   cy.get('@page').click({ force: true });
-// });
+
+
+Cypress.Commands.add('navigateNavBar', (page) => {
+  cy.get('[data-test-id="navbarMainMenu"]').contains(page).parent().as('page');
+  cy.get('@page').click({ force: true });
+});
 
 // Cypress.Commands.add("shouldNotBeActionable", { prevSubject: "element" }, (subject, done) => {
 //   cy.once("fail", (err) => {
