59ed9dfbb419e5ef57b6527033201f6e7de29a58
Nurangiz Aliyeva <nurangiz.aliyeva@Loaners-MacBook-Pro.local>
2022-07-29 08:46:26 +1000

    added more tests

--- cypress/e2e/dummyTestFile.spec.js
+++ cypress/e2e/dummyTestFile.spec.js
@@ -1,45 +1,24 @@
 // Create your test file here
 it('visit GU website', () => {
     cy.visit('/')
-    cy.get('cerberus-navbar-main-menu-item gu-simple-text').contains('expansion').click()
+    cy.get('cerberus-navbar-main-menu-item gu-simple-text').contains('market').click()
+    cy.get('cerberus-navbar-main-menu-sub-item li').contains('star store').click()
 })
 
-it('expansion', () => {
-    it('How to use env varibales', () => {
-        const trial = Cypress.env('POWERFUL CHAMPIONS, PRIZE DRAWS @ NEW WAYS TO PLAY');
-    })
-    it('expansion', () => {
-        it('How to use env varibales', () => {
-            const divine = Cypress.env('PLAY GODS UNCHAINED'); (SOLD);
+it('login'), () => {
+    it('How to use env varibales'), () => {
+        const username = Cypress.env('USERNAME');
+        const password = Cypress.env('PASSWORD');
 
-            it('visit GU website', () => {
-                cy.visit('/')
-                cy.get('cerberus-navbar-main-menu-item gu-simple-text').contains('expansion').click()
-                cy.get('cerberus-navbar-main-menu-sub-item li').contains('mortal judgment').click()
-            })
-            it('expansion', () => {
-                it('How to use env varibales', () => {
-                    const mortal = Cypress.env('PLAY GODS UNCHAINED'); ('BUY CARD PACKS');
+        cy.intercept('POST', '**/auth2*').as('authenticateCredentials');
 
-                    /////////////////////////////////////////////////////////////////////////////////
-                    it('visit GU website', () => {
-                        cy.visit('/')
-                        cy.get('cerberus-navbar-main-menu-item gu-simple-text').contains('market').click()
-                        cy.get('cerberus-navbar-main-menu-sub-item li').contains('star store').click()
-                    })
+        cy.get('[data-test-id="emailInput2"]').click().type(username);
+        cy.get('[data-test-id="passwordInput2" ]').click().type(password);
+        cy.get('[data-test-id="loginButton"]').click({ force: true });
+        cy.wait('@authenticateCredentials')
+            .its('response.statusCode')
+            .should('be.oneOf', [406, 401, 200])
+            .then(recapture('@authenticateCredentials'));
 
-                    it('login', () => {
-                        it('How to use env varibales', () => {
-                            const username = Cypress.env('USERNAME');
-                            const password = Cypress.env('PASSWORD');
-
-                            cy.intercept('POST', '**/auth2*').as('authenticateCredentials');
-
-                            cy.get('[data-test-id="emailInput2"]').click().type(username);
-                            cy.get('[data-test-id="passwordInput2" ]').click().type(password);
-                            cy.get('[data-test-id="loginButton"]').click({ force: true });
-                            cy.wait('@authenticateCredentials')
-                                .its('response.statusCode')
-                                .should('be.oneOf', [406, 401, 200])
-                                .then(recapture('@authenticateCredentials'));
-                        })
+    }
+}
